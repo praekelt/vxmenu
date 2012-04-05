c39c5ad118b0f7449c8b7ed5de612dba8a0756ca
Shaun Sephton <shaunsephton@gmail.com>
2012-04-05 13:18:09 +0200

    reorganize methods

--- vxmenu/menu.py
+++ vxmenu/menu.py
@@ -9,34 +9,6 @@ class Menu(object):
     text = None
     options = []
 
-    def get_text(self):
-        """
-        By default menu text (header) is defined in self.text.
-        This allows for dynamic text creation through method overriding.
-        """
-        return self.text
-
-    def get_options(self):
-        """
-        By default menu options is defined in self.options.
-        This allows for dynamic option creation through method overriding.
-        """
-        return self.options
-
-    def process_response(self, choice):
-        """
-        Override to perform any additional menu specific processing.
-        """
-        pass
-
-    def generate_menu(self, choice):
-        """
-        Generates menu based on determined text and options.
-        """
-        options = self.response_menu.get_options()
-        return self.response_menu.get_text() + '\n' + '\n'.join(
-            ['%s. %s' % (idx, opt[0]) for idx, opt in enumerate(options, 1)])
-
     def __init__(self, message, session, choice=None):
         """
         Initiate menu based on incomming choice from previous menu.
@@ -55,3 +27,31 @@ class Menu(object):
                     load_class_by_string(resolved_choice[2])(message, session)
         else:
             self.response_menu = self
+
+    def generate_menu(self, choice):
+        """
+        Generates menu based on determined text and options.
+        """
+        options = self.response_menu.get_options()
+        return self.response_menu.get_text() + '\n' + '\n'.join(
+            ['%s. %s' % (idx, opt[0]) for idx, opt in enumerate(options, 1)])
+
+    def get_options(self):
+        """
+        By default menu options is defined in self.options.
+        This allows for dynamic option creation through method overriding.
+        """
+        return self.options
+
+    def get_text(self):
+        """
+        By default menu text (header) is defined in self.text.
+        This allows for dynamic text creation through method overriding.
+        """
+        return self.text
+
+    def process_response(self, choice):
+        """
+        Override to perform any additional menu specific processing.
+        """
+        pass
