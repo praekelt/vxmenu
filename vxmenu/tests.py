76650859909003340b14050ac91262831bf28fab
Shaun Sephton <shaunsephton@gmail.com>
2012-04-05 13:17:47 +0200

    test scaffold

--- vxmenu/tests.py
+++ vxmenu/tests.py
@@ -1,6 +1,29 @@
 import unittest
 
 
-class DummyTestCase(unittest.TestCase):
-    def test_test(self):
-        raise NotImplementedError("No tests yet!")
+class MenuApplicationWorkerTestCase(unittest.TestCase):
+    def test_consume_user_message(self):
+        raise NotImplementedError()
+
+    def test_start_worker(self):
+        raise NotImplementedError()
+
+    def test_stop_worker(self):
+        raise NotImplementedError()
+
+
+class MenuTestCase(unittest.TestCase):
+    def test_init(self):
+        raise NotImplementedError()
+
+    def test_generate_menu(self):
+        raise NotImplementedError()
+
+    def test_get_options(self):
+        raise NotImplementedError()
+
+    def test_get_text(self):
+        raise NotImplementedError()
+
+    def test_process_response(self):
+        raise NotImplementedError()
